8b3131cdee1b11af52533dd9a651f5c5d6cfb67c
Administrator <eduard@4dclick.kz>
2018-11-27 22:49:27 +0600

    Add django, start django models

---
+++ web/contester/models/test.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .exercise import Exercise
+from .user import User
+
+
+class Test(models.Model):
+    successfully = models.BooleanField()
+    message = models.CharField(max_length=50)
+    exercise = models.ForeignKey(Exercise, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        return self.name
